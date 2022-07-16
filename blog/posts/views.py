7dc74b0fbea0d59795fc77197376cdd47073413e
unhaten <csgold2002@mail.ru>
2022-07-16 16:34:00 +0300

    first ver of blog

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+
+def index(request):
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts': posts})
